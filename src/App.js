d613663b643d97266de29508abd6a9fd4fafae28
Mitchell Rhoads <mitchelldrhoads@gmail.com>
2019-10-28 18:18:02 -0700

    added button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}from 'react';
 import './App.css';
 
-function App() {
+
+class App extends Component{
+state ={
+  numberOfLikes: 0,
+  like: "Likes"
+
+}
+handleClick=(e)=>{
+  // this.setState(e => {
+  //   return {numberOfLikes: e.numberOfLikes +1}
+  // })
+  // if(this.state.numberOfLikes === 0){
+  //   this.setState(e => {
+  //     return { like : e.like = "Like"}
+  //   })
+  // } else{
+  //   this.setState(e => {
+  //     return { like: e.like = "Likes"}
+  //   })
+  // }
+
+  this.setState({
+    numberOfLikes: this.state.numberOfLikes +1,
+    like: this.state.numberOfLikes ===0 ? "Like": "Likes"
+  })
+}
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="button">
+      <button onClick={this.handleClick}>{this.state.numberOfLikes} {this.state.like}</button>
     </div>
   );
 }
-
+}
 export default App;
